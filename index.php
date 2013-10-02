b3944fbd7c5d4de6c3d0bffd783bf09aec0a2c01
Michael Norris <github@michaelnorris.co.uk>
2013-10-02 07:21:17 +0100

    Index page added

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Short description for file
+ *
+ * Long description for file (if any)...
+ *
+ * @category   CategoryName
+ * @package    PackageName
+ * @author     Michael Norris <mike@michaelnorris.co.uk>
+ * @copyright  2013 Michael Norris
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id:$
+ * @link       http://pear.php.net/package/PackageName
+ * @see        NetOther, Net_Sample::Net_Sample()
+ * @since      File available since Release 1.2.0
+ * @deprecated File deprecated in Release 2.0.0
+ */
+
+?>
